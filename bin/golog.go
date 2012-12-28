b251f43880b4eb4ef3b30f2ea9f6b049cff6e74e
Michael Hendricks <michael@ndrix.org>
2012-12-28 13:07:43 -0700

    Deprecate golog helper for tests

--- bin/golog.go
+++ bin/golog.go
@@ -7,11 +7,5 @@ import . "fmt"
 import . "github.com/mndrix/golog"
 
 func main () {
-    db0 := NewDatabase()
-    db1 := db0.Asserta(NewTerm("alpha"))
-    db2 := db1.Asserta(NewTerm("beta"))
-    db3 := db2.Asserta(NewTerm("gamma", NewTerm("greek to me")))
-    Printf("db1:\n%s\n", db1.String())
-    Printf("db2:\n%s\n", db2.String())
-    Printf("db3:\n%s\n", db3.String())
+    Printf("hi")
 }
