4a21f850df6256bdc942289416286ed2f1aaf08c
Michael Hendricks <michael@ndrix.org>
2013-03-20 13:28:14 -0600

    Make profile.sh more flexible
    I can now use it to generate either CPU or memory profiles.  I can
    also specify which benchmarks should be included in the profile.

--- profile.sh
+++ profile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 go test -c
-./golog.test -test.run=none -test.bench=. -test.cpuprofile=cpu.profile -test.memprofile=mem.profile
+./golog.test -test.run=none -test.bench=$2 -test.$1profile=$1.profile
